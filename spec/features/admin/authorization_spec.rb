8f0c276873690b440bfcae51241ba8c8cd06a3fe
Matt Levy <milevy1@gmail.com>
2019-05-07 16:13:06 -0600

    Added admin authorization spec

---
+++ spec/features/admin/authorization_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'Admin authorization' do
+  scenario 'Non admin users cannot access Admin paths' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user).and_return(user)
+
+    expect{visit admin_dashboard_path}
+      .to raise_error(ActionController::RoutingError)
+  end
+end

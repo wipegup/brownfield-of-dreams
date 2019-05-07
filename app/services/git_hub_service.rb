218fc8a1eb39a31db3b3291a9345488a121c9085
Matt Levy <milevy1@gmail.com>
2019-05-07 15:57:49 -0600

    Added space before equal, changed to single quotes, removed unused variable, added class description comment to header

--- app/services/git_hub_service.rb
+++ app/services/git_hub_service.rb
@@ -1,18 +1,18 @@
+# Service helper to connect and parse Github API data
 class GitHubService
-
   def initialize(token)
     @token = token
   end
 
   def get_repos
     response = conn.get('repos')
-    data = JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def conn
-    Faraday.new("https://api.github.com/user/") do |f|
+    Faraday.new('https://api.github.com/user/') do |f|
       f.adapter Faraday.default_adapter
-      f.params['access_token']= @token
+      f.params['access_token'] = @token
     end
   end
 end

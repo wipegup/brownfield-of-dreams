f08b2ea201b1b0b0c8d3ebf88892869252e23473
Matt Levy <milevy1@gmail.com>
2019-05-07 16:14:51 -0600

    Changed to single quotes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,8 +13,8 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
-  config.filter_sensitive_data("<GH_USER_TOKEN>") { ENV['GH_USER_TOKEN'] }
+  config.filter_sensitive_data('<YOUTUBE_API_KEY>') { ENV['YOUTUBE_API_KEY'] }
+  config.filter_sensitive_data('<GH_USER_TOKEN>') { ENV['GH_USER_TOKEN'] }
 
   config.ignore_hosts(
   "chromedriver.storage.googleapis.com",

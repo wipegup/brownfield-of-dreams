791000d20df538dd457f128659e68ddf337690d5
Matt Levy <milevy1@gmail.com>
2019-05-07 16:13:06 -0600

    Deleted tutorial_name helper method because it was unused

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,11 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :list_tags
-  helper_method :tutorial_name
 
   add_flash_types :success
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def tutorial_name(id)
-    Tutorial.find(id).title
   end
 
   def four_oh_four

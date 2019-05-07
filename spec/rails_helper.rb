b522a47eacb9cf69d90166ca4fc6ab786ace8cdd
Matt Levy <milevy1@gmail.com>
2019-05-07 16:13:06 -0600

    Refactored GH_USER_TOKEN to not be hardcoded
    Co-authored-by: William Peterson <29714514+wipegup@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,7 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
+  config.filter_sensitive_data("<GH_USER_TOKEN>") { ENV['GH_USER_TOKEN'] }
 
   config.ignore_hosts(
   "chromedriver.storage.googleapis.com",

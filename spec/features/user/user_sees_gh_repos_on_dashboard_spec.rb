2af2cd1edc8e777a1cc2a30360cad8c0b2b20ca7
wipegup <wipegup@gmail.com>
2019-05-08 12:05:16 -0600

    Add test for followers

--- spec/features/user/user_sees_gh_repos_on_dashboard_spec.rb
+++ spec/features/user/user_sees_gh_repos_on_dashboard_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 
-describe 'A registered user' do
-  it 'can see five repos', :vcr do
-    user = create(:user)
+describe 'A registered user',:vcr do
+  before :each do
+    @user = create(:user)
 
     allow_any_instance_of(ApplicationController)
-      .to receive(:current_user).and_return(user)
-    allow(user).to receive(:token).and_return(ENV['GH_USER_TOKEN'])
-
+      .to receive(:current_user).and_return(@user)
+    allow(@user).to receive(:token).and_return(ENV['GH_USER_TOKEN'])
     visit dashboard_path
+  end
+
+  it 'can see five repos'do
 
     within('#github-repos') do
       expect(page).to have_css('.repo-link', count: 5)
+      within(first('.repo-link')) do
+        expect(page).to have_selector('a')
+      end
+    end
+  end
+
+  it 'can see all followers' do
+
+    within('#github-followers') do
+      expect(page).to have_css('.follower-link')
       within(first('.repo-link')) do
         expect(page).to have_selector('a')
       end

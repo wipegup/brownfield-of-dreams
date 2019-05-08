e69f44ecb44f601634df4fda455492c9cfdd9c55
wipegup <wipegup@gmail.com>
2019-05-08 16:48:09 -0600

    Add tests for editing and deleting videos from tutorials

--- spec/features/admin/edit_tutorial_spec.rb
+++ spec/features/admin/edit_tutorial_spec.rb
@@ -1,26 +1,66 @@
 require 'rails_helper'
 
-describe 'An Admin can edit a tutorial' do
-  let(:tutorial) { create(:tutorial) }
-  let(:admin)    { create(:admin) }
-
-  scenario 'by adding a video', :js, :vcr do
+describe 'An Admin' do
+  before :each do
+    @tutorial = create(:tutorial)
+    @admin = create(:admin)
     allow_any_instance_of(ApplicationController)
-      .to receive(:current_user).and_return(admin)
+      .to receive(:current_user).and_return(@admin)
+  end
 
-    visit edit_admin_tutorial_path(tutorial)
+  before :each, editable_tutorial: true do
+    @videos = create_list(:sequenced_video, 3, tutorial:@tutorial)
+  end
 
-    click_on 'Add Video'
+  describe 'can edit a tutorial' do
 
-    fill_in 'video[title]', with: 'How to tie your shoes.'
-    fill_in 'video[description]', with: 'Over, under, around and through'
-    fill_in 'video[video_id]', with: 'J7ikFUlkP_k'
-    click_on 'Create Video'
+    scenario 'by adding a video', :js, :vcr do
+      visit edit_admin_tutorial_path(@tutorial)
 
-    expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
+      click_on 'Add Video'
 
-    within(first('.video')) do
-      expect(page).to have_content('How to tie your shoes.')
+      fill_in 'video[title]', with: 'How to tie your shoes.'
+      fill_in 'video[description]', with: 'Over, under, around and through'
+      fill_in 'video[video_id]', with: 'J7ikFUlkP_k'
+      click_on 'Create Video'
+
+      expect(current_path).to eq(edit_admin_tutorial_path(@tutorial))
+
+      within(first('.video')) do
+        expect(page).to have_content('How to tie your shoes.')
+      end
     end
+
+    scenario 'by going to a page to edit video attributes', :editable_tutorial do
+      visit edit_admin_tutorial_path(@tutorial)
+
+      within('#video-list') do
+        video_id = first('div', class:'video')['data-id']
+        within(first('div', class:'video')) do
+          click_on "Edit"
+        end
+      end
+
+      expect(current_path).to eq(edit_admin_video_path(Video.find(video_id)))
+    end
+
+    scenario 'by deleting video from the tutorial', :editable_tutorial do
+      visit edit_admin_tutorial_path(@tutorial)
+
+      within('#video-list') do
+        video_id = first('div', class:'video')['data-id']
+        video_info = Video.find(video_id)
+        within(first('div', class:'video')) do
+          click_on "Delete"
+        end
+      end
+
+      expect(current_path).to eq(edit_admin_tutorial_path(@tutorial))
+
+      expect(page).not_to have_content(video_info.title)
+    end
+
+    scenario 'by moving videos around by dragging them'
+
   end
 end

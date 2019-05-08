6a6c5eda69be5940eac7cd71af498c545223c548
Matt Levy <milevy1@gmail.com>
2019-05-08 12:17:44 -0600

    Refactored admin authorization spec

--- spec/features/admin/authorization_spec.rb
+++ spec/features/admin/authorization_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 describe 'Admin authorization' do
-  scenario 'Non admin users cannot access Admin paths' do
+  scenario 'Unregistered users cannot access Admin paths' do
+    visit admin_dashboard_path
+
+    expect(page.status_code).to eq(404)
+  end
+
+  scenario 'Registered users cannot access Admin paths' do
     user = create(:user)
     allow_any_instance_of(ApplicationController)
       .to receive(:current_user).and_return(user)
 
-    expect { visit admin_dashboard_path }
-      .to raise_error(ActionController::RoutingError)
-  end
+    visit admin_dashboard_path
+
+    expect(page.status_code).to eq(404)
+  end  
 end

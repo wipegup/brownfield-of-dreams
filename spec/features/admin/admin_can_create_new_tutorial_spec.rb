878d274a1508fb9867b5868f43a64f1d5b43ccd4
wipegup <wipegup@gmail.com>
2019-05-08 06:17:44 -0600

    add test for adding tutorial as admin

---
+++ spec/features/admin/admin_can_create_new_tutorial_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'An Admin' do
+  it 'can add a tutorial', :js do
+    admin = create(:admin)
+    new_tutorial_attrs = attributes_for(:tutorial)
+
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user).and_return(admin)
+
+    visit admin_dashboard_path
+    click_on 'New Tutorial'
+
+    expect(current_path).to eq(new_admin_tutorial_path)
+
+    fill_in 'tutorial[title]', with: new_tutorial_attrs['title']
+    fill_in 'tutorial[description]', with: new_tutorial_attrs['description']
+    fill_in 'tutorial[thumbnail]', with: new_tutorial_attrs['thumbnail']
+
+    click_on 'Save'
+
+    expect(current_path).to eq(admin_dashboard_path)
+    expect(page).to have_content(new_tutorial_attrs['title'])
+    expect(page)
+      .to have_xpath("//img[@src='#{new_tutorial_attrs['thumbnail']}']")
+  end
+end

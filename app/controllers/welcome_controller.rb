548d7d0a3be32d93e5dd72f384a7d3ace7d45e3a
wipegup <wipegup@gmail.com>
2019-05-08 14:55:24 -0600

    Tutorials hidden in welcome view to visitors if marked as classroom

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,19 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag])
-                           .paginate(page: params[:page], per_page: 5)
+    if current_user
+      if params[:tag]
+        @tutorials = Tutorial.tagged_with(params[:tag])
+                             .paginate(page: params[:page], per_page: 5)
+      else
+        @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
+      end
     else
-      @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
+      if params[:tag]
+        @tutorials = Tutorial.tagged_with(params[:tag]).where(classroom: false)
+                             .paginate(page: params[:page], per_page: 5)
+      else
+        @tutorials = Tutorial.where(classroom: false).paginate(page: params[:page], per_page: 5)
+      end
     end
   end
 end

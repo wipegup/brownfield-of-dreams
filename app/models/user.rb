06fc19ed9c864b584e25df7337d56f831b3cb426
Matt Levy <milevy1@gmail.com>
2019-05-09 15:02:26 -0600

    Refactored user password validation to only run on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,8 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
+  validates_presence_of :password, on: :create
   validates_presence_of :first_name
   enum role: %i[default admin]
   has_secure_password
-
-  def token
-    nil
-  end
 end

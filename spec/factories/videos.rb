23a47da95398a75f42f5a4a6aaae3554abee4157
Matt Levy <milevy1@gmail.com>
2019-05-09 11:21:42 -0600

    Updated video and user_video factories

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     title { Faker::Pokemon.name }
     description { Faker::SiliconValley.motto }
     video_id { Faker::Crypto.md5 }
+    thumbnail { 'https://media.licdn.com/dms/image/C560BAQFWOfDVm7nHeg/company-logo_200_200/0?e=2159024400&v=beta&t=L7yzPfsyYeZUjkJc1Abfwbg-Nx710fAvwEYbf02LIEE' }
     tutorial
   end
 end

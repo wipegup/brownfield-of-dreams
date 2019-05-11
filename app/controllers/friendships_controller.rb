aae4adb42fee1a67d59a2de043aeba25f902e62e
Matt Levy <milevy1@gmail.com>
2019-05-11 16:23:34 -0600

    Added FriendshipsController with create action

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,9 @@
+class FriendshipsController < ApplicationController
+  def create
+    new_friend = User.find_by(github_uid: params[:github_uid])
+
+    current_user.friendships.create(friend_id: new_friend.id)
+
+    redirect_to dashboard_path
+  end
+end

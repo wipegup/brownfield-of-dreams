0d437f6cb421573c6a02f83344e2075172a47cd9
Matt Levy <milevy1@gmail.com>
2019-05-11 16:24:06 -0600

    Added to GithubUser model the github_uid and user_id

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,7 +1,13 @@
 class GithubUser
-  attr_reader :name, :link
+  attr_reader :name, :link, :github_uid, :user_id
   def initialize(data)
     @name = data[:login]
     @link = data[:html_url]
+    @github_uid = data[:id]
+    @user_id = User.find_by(github_uid: data[:id])
+  end
+
+  def has_account?
+    user_id ? true : false
   end
 end

e2682b9f3acf2870abacba7c04c0952600377afc
Matt Levy <milevy1@gmail.com>
2019-05-15 18:42:15 -0600

    Added spec for a user to send an email invitation
    Co-authored-by: William Peterson <29714514+wipegup@users.noreply.github.com>

---
+++ spec/features/user/user_can_send_email_invitations_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'Logged in user', :vcr do
+  before :each do
+    @user = create(:user)
+
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user).and_return(@user)
+    allow(@user).to receive(:github_token).and_return(ENV['GH_USER_TOKEN'])
+    allow(@user).to receive(:github_uid).and_return(true)
+
+    visit dashboard_path
+
+    click_on 'Invite a Friend to Join'
+  end
+
+  it 'can email invitations to friends' do
+    expect(current_path).to eq('/invite')
+
+    fill_in 'github_username', with: 'milevy1'
+    click_on 'Send Invite'
+
+    ## Try out using `ActionMailer::Base.deliveries` for testing?
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content('Successfully sent invite!')
+  end
+
+  it 'shows error message if user does not have an email available' do
+    fill_in 'github_username', with: 'wipegup'
+    click_on 'Send Invite'
+
+    expect(page).to have_content('The Github user you selected does not have an email address associated with their account.')
+  end
+end

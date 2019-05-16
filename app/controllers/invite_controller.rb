f67fd21c666da861b9ca7c7a88e1360e0e0df3a4
Matt Levy <milevy1@gmail.com>
2019-05-15 18:49:40 -0600

    Adds invite controller to generate email to friend
    Co-authored-by: William Peterson <29714514+wipegup@users.noreply.github.com>

---
+++ app/controllers/invite_controller.rb
@@ -0,0 +1,19 @@
+class InviteController < ApplicationController
+  def new
+  end
+
+  def create
+    service = GitHubService.new(current_user.github_token).user_info(params[:github_username])
+    invitee_email = service[:email]
+    invitee_name = service[:name]
+
+    if invitee_email
+      FriendInviteMailer.invite(current_user, invitee_email, invitee_name).deliver_now
+      flash[:notice] = 'Successfully sent invite!'
+    else
+      flash[:notice] = 'The Github user you selected does not have an email address associated with their account.'
+    end
+    
+    redirect_to dashboard_path
+  end
+end

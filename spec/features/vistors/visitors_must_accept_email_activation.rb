92d6c92437a5ab7aa9078c783700e08e95fdf40c
Matt Levy <milevy1@gmail.com>
2019-05-15 18:51:34 -0600

    Adds spec for visitor to activate new account by email link

---
+++ spec/features/vistors/visitors_must_accept_email_activation.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe 'visitor can create an account' do
+  before :each do
+    @email = 'jimbob@aol.com'
+    @first_name = 'Jim'
+    @last_name = 'Bob'
+    @password = 'password'
+    @password_confirmation = 'password'
+  end
+
+  it 'must accept email activation code' do
+    visit '/'
+
+    click_on 'Sign In'
+
+    expect(current_path).to eq(login_path)
+
+    click_on 'Sign up now.'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in 'user[email]', with: @email
+    fill_in 'user[first_name]', with: @first_name
+    fill_in 'user[last_name]', with: @last_name
+    fill_in 'user[password]', with: @password
+    fill_in 'user[password_confirmation]', with: @password
+
+    click_on'Create Account'
+
+    expect(current_path).to eq(dashboard_path)
+
+    expect(page).to have_content(@email)
+    expect(page).to have_content(@first_name)
+    expect(page).to have_content(@last_name)
+    expect(page).to_not have_content('Sign In')
+
+    msg = 'This account has not yet been activated. Please check your email.'
+
+    expect(page).to have_content(msg)
+    expect(page).to have_content("Logged in as #{@email}")
+
+    # As a non-activated user
+    # When I check my email for the registration email
+    # I should see a message that says "Visit here to activate your account."
+    # And when I click on that link
+    activation_email_code = Activation.first.email_code
+    visit activation_path(activation_email_code)
+
+    # Then I should be taken to a page that says "Thank you! Your account is now activated."
+    expect(current_path).to eq(activate_path(activation_email_code))
+    expect(page).to have_content('Thank you! Your account is now activated.')
+
+    # And when I visit "/dashboard"
+    # Then I should see "Status: Active"
+    visit dashboard_path
+    expect(page).to have_content('Status: Active')
+  end
+end
